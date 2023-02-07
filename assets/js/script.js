fc4b2cad5d5925e8136232e4bc29855e5bce9c92
Gabriel Felix <gabrielfelixjose@gmail.com>
2023-02-07 15:37:07 -0300

    ajustes no js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-function mostraMenu() {
-  let menu = document.querySelector(".menu-mobile");
-  let blur = document.querySelector(".blur");
+function _mostraMenu() {
+  const menu = document.querySelector(".menu-mobile");
+  const blur = document.querySelector(".blur");
 
   menu.classList.toggle("ativo");
   blur.classList.toggle("blur-on");
